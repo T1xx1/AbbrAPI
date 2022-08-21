62ab0144baf96bd5dd8b41c856b029218c511ed4
Tiziano Perego <69322987+T1xx1@users.noreply.github.com>
2022-08-21 23:29:31 +0200

    Added error handling

--- routes/abbr.js
+++ routes/abbr.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const words = require('../data/words');
 
 router.get('/', (req, res) => {
-   let text = ` ${req.body.text.toLowerCase()} `;
+   let text = ` ${req.body.text} `;
+
+   if (text === ' undefined ') res.status('400').send('text param required');
 
    for (let word in words) text = text.replaceAll(` ${word} `, words[word]);
 
--- routes/text.js
+++ routes/text.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const words = require('../data/words');
 
 router.get('/', (req, res) => {
-   let abbr = ` ${req.body.abbr.toLowerCase()} `;
+   let abbr = ` ${req.body.abbr} `;
+
+   if (abbr === ' undefined ') res.status('400').send('abbr param required');
 
    for (let word in words) abbr = abbr.replaceAll(` ${words[word]} `, word);
 
